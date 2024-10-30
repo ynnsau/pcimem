79dfbbccbed0fbfb535d914168012fa1096c38ac
Yan Sun <yans3@illinois.edu>
2024-10-29 22:55:52 -0500

    adjusting ar/awuser bits for m5 hppb

--- set_all_init.sh
+++ set_all_init.sh
@@ -2,4 +2,5 @@ set -x
 bash set_pa_offset.sh
 bash set_range.sh
 bash set_user_bits.sh
-bash set_clk_default.sh
+#bash set_clk_default.sh
+bash set_traffic_default.sh
--- set_user_bits.sh
+++ set_user_bits.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 set -x
 sudo setpci -s 27:00.1 COMMAND=0x02
-#awuser 0x22 -- 10 0010 -- HDM, host-bias, nc-p
-#aruser 0x22 -- 10 0010 -- HDM, host-bais, cache-own read
-sudo /home/yans3/gitdoc/pcimem/pcimem /sys/devices/pci0000\:27/0000\:27\:00.1/resource2 104 d 0x2200000022
+#awuser 0x22 -- 10 0010 -- HDM, host-bias, nc-p // hot addr, hot page (bot host)
+#aruser 0x22 -- 10 0010 -- HDM, host-bais, cache-own read // hot page read (hard coded), read src addr ()
+sudo /home/yans3/gitdoc/pcimem/pcimem /sys/devices/pci0000\:27/0000\:27\:00.1/resource2 104 d 0x0200000002
 
 #awuser 0x21 -- 10 0001 -- HDM, host-bias, cache-own write
 #aruser 0x22 -- 10 0010 -- HDM, host-bais, cache-own read
